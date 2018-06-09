fc48b22c952f365a74c2728a6dc2c57d906ce538
ShashKash <shashankkashyap2208@gmail.com>
2018-06-09 17:31:25 +0530

    Added DB mechanism

--- FlaskAPI/routes.py
+++ FlaskAPI/routes.py
@@ -1,6 +1,6 @@
 import os
 from flask import render_template, request, send_from_directory
-from FlaskAPI import app, original_target, colored_target
+from FlaskAPI import app, db, original_target, colored_target
 from FlaskAPI.models import Images
 from FlaskAPI.colorize import colorize
 
@@ -13,20 +13,30 @@ def upload():
     file = request.files['file']
     print(file)
     ori_name,extension = os.path.splitext(file.filename)
-    print(ori_name)
-    print(extension)
-    ori_storage_name = str(int(len([i for i in os.listdir(original_target)]))+1)+extension
-    print(ori_storage_name)
+    #print(ori_name)
+    #print(extension)
+
+## Saving the b/w image in original_target##
+    ori_storage_name = str(Images.query.count()+1)+extension
     destination_bw = "/".join([original_target,ori_storage_name])
     print(destination_bw)
-    col_storage_name = 'col_' + ori_storage_name
+    col_storage_name = 'col_' + str(Images.query.count()+1) + '.png'
     file.save(destination_bw)
     destination_col = "/".join([colored_target,col_storage_name])
     print(destination_col)
 
-    colorize(img_in=destination_bw,img_out=destination_col)
+## Storing the info about image in database##
+    image = Images(original_name=file.filename, stored_name=col_storage_name, original_image=destination_bw, colored_image=destination_col)
+    db.session.add(image)
+    db.session.commit()
 
-    return render_template("image_show.html", image_name=col_storage_name)
+## Using the DL model to colorize the image ##
+## and save it in colored_target ##
+    colorize(img_in=image.original_image,img_out=image.colored_image)
+
+## Returning the required fetch info ##
+## to user for the next step ##
+    return render_template("image_show.html", image_name=image.stored_name)
 
 @app.route("/colored/<filename>")
 def getColored(filename):
